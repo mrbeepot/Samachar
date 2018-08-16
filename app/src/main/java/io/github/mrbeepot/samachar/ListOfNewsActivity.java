f4fc4c5c22075c06754ac826d44dc7c3335ea352
unknown <36437651+mrbeepot@users.noreply.github.com>
2018-08-17 00:24:46 +0530

    Initial commit

---
+++ app/src/main/java/io/github/mrbeepot/samachar/ListOfNewsActivity.java
@@ -0,0 +1,13 @@
+package io.github.mrbeepot.samachar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListOfNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_news);
+    }
+}
